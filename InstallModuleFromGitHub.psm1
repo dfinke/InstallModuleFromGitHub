3c3972999f1904f4441f44bdb06c94dd5da3a333
Jesse Gigler <jessegigler@gmail.com>
2017-02-09 19:23:09 -0500

    some cleanup (join-path) and a few tweaks to get this to work on OSX and Ubuntu 16.04

--- InstallModuleFromGitHub.psm1
+++ InstallModuleFromGitHub.psm1
@@ -2,7 +2,7 @@ function Install-ModuleFromGitHub {
     [CmdletBinding()]
     param(
         $GitHubRepo,
-        $Branch="master",
+        $Branch = "master",
         [Parameter(ValueFromPipelineByPropertyName)]
         $ProjectUri,
         $DestinationPath
@@ -10,9 +10,9 @@ function Install-ModuleFromGitHub {
 
     Process {
         if($PSBoundParameters.ContainsKey("ProjectUri")) {
-            $GitHubRepo=$null
+            $GitHubRepo = $null
             if($ProjectUri.OriginalString.StartsWith("https://github.com")) {
-                $GitHubRepo=$ProjectUri.AbsolutePath
+                $GitHubRepo = $ProjectUri.AbsolutePath
             } else {
                 $name=$ProjectUri.LocalPath.split('/')[-1]
                 Write-Host -ForegroundColor Red ("Module [{0}]: not installed, it is not hosted on GitHub " -f $name)
@@ -21,38 +21,49 @@ function Install-ModuleFromGitHub {
 
         if($GitHubRepo) {
                 Write-Verbose ("[$(Get-Date)] Retrieving {0} {1}" -f $GitHubRepo, $Branch)
+                Write-Debug "PSModulePath: $PSModulePath"
 
-                $url="https://github.com/{0}/archive/{1}.zip" -f $GitHubRepo, $Branch
+                $url = "https://github.com/{0}/archive/{1}.zip" -f $GitHubRepo, $Branch
                 $targetModuleName=$GitHubRepo.split('/')[-1]
 
-                $OutPath="$([System.IO.Path]::GetTempPath())\$targetModuleName"
-
+                $OutPath = Join-Path -Path "$([System.IO.Path]::GetTempPath())" -ChildPath "$targetModuleName"
+                Write-Debug "OutPath: $OutPath"
                 if(!(Test-Path $OutPath)) {
-                    $null=md $OutPath
+                    $null = md $OutPath
                 }
 
-                $OutFile="$($OutPath)\$($Branch).zip"
-                
+                $OutFile = Join-Path -Path $OutPath -ChildPath "$($Branch).zip"
+                Write-Debug "OutFile: $OutFile"
+
                 Invoke-RestMethod $url -OutFile $OutFile
-                Unblock-File $OutFile
+                if ($IsWindows) {
+                  Unblock-File $OutFile
+                }
                 Expand-Archive -Path $OutFile -DestinationPath $OutPath -Force
 
-                $targetModule="\$($targetModuleName)-$($Branch)"
-                
-                $dest="C:\Program Files\WindowsPowerShell\Modules"
-                if($DestinationPath) {
-                    $dest=$DestinationPath
-                }
-                $dest+="\$targetModuleName"
+                $targetModule = Join-Path -Path $targetModuleName -ChildPath $Branch
 
-                $psd1=ls $OutPath *.psd1 -Recurse
-                
+                if ($IsWindows) {
+                  $dest = "C:\Program Files\WindowsPowerShell\Modules"
+                }
+
+                else {
+                  $dest = Join-Path -Path $HOME -ChildPath ".local/share/powershell/Modules"
+                }
+
+                if($DestinationPath) {
+                    $dest = $DestinationPath
+                }
+                $dest = Join-Path -Path $dest -ChildPath $targetModuleName
+
+                $psd1 = Get-ChildItem $OutPath -Include *.psd1 -Recurse
+
                 if($psd1) {
                     $ModuleVersion=(Get-Content -Raw $psd1.FullName | Invoke-Expression).ModuleVersion
-                    $dest+="\$($ModuleVersion)"
-                }        
-                
-                $null=Robocopy.exe "$($OutPath)\$($targetModule)" $dest /mir
+                    $dest = Join-Path -Path $dest -ChildPath $ModuleVersion
+                }
+
+                $null = Copy-Item "$(Join-Path -Path $OutPath -ChildPath $targetModule)/*" $dest -Force
         }
     }
 }
